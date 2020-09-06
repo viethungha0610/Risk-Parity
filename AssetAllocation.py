f936bde0c0f0bcf440c7e3404722ba5080e7caa6
viethungha0610 <viethungha0610@gmail.com>
2020-09-06 15:26:51 +0100

    060920
    Simplified the folder significantly;
    Added docstrings;

--- AssetAllocation.py
+++ AssetAllocation.py
@@ -1,3 +1,4 @@
+# Necessary packages for the programme to run
 import pandas as pd
 import numpy as np
 import sympy
@@ -12,6 +13,9 @@ import time
 
 # Parent class
 class RiskParity():
+    """
+    This is the main Risk Parity class, where main operations such as calculating the risk function
+    """
     from scipy.optimize import minimize
     import numpy as np
     def __init__(self, 
@@ -20,19 +24,33 @@ class RiskParity():
                  w_guess=None):
         """Inserting data into class"""
         """Following guidance in Braga (2016), the class will by default favour optimisation via SQP"""
-        # Making sure len(assets) == len(cov_mat.shape[0])
+        """
+        Args:
+            cov_mat (array): the return covariance matrix 
+            assets (list): list of asset names or ticker 
+            w_guess (list or array): initial guess for Risk Parity asset allocation, if not specified the default will be equal weights 
+
+        Raises:
+            ValueError: if the number of assets is not equal to the number of rows/columns of the covariance matrix.
+        """
         self.cov_mat = cov_mat
         self.assets = assets
         if w_guess is None:
             w_guess=np.full((self.cov_mat.shape[0]), 1/self.cov_mat.shape[0])
             self.w_guess = w_guess
         self.w_guess = w_guess
-        # Error handling
+        # Error handling: Making sure len(assets) == len(cov_mat.shape[0])
         if len(self.assets) != self.cov_mat.shape[0]:
-            raise ValueError('Number of assets must be equal to number of rows/columns in the covariance matrix')
+            raise ValueError('Number of assets must be equal to number of rows/columns of the covariance matrix')
 
     def risk_func(self, w=None):
-        """Main function to minimise"""
+        """Main risk function to minimise - convex risk function. In this class, this is a helper function.
+        Args:
+            w (list or array): initial guess for Risk Parity asset allocation, if not specified the default will be equal weights (list or array)
+
+        Returns:
+            risk_func (float): the calculated risk function
+        """
         # Start off with vector x
         if w is None:
             w = self.w_guess
@@ -46,10 +64,19 @@ class RiskParity():
         return risk_func
     
     def optimize(self, assets=None, method='SLSQP'):
-        """Returns an risk parity asset allocation"""
-        """Attribute: 
-        - allocation_ -> Risk Parity allocation
-        - minimised_val_ -> Minimised risk function value"""
+        """This method minimize the risk function (risk_func) and displays a Risk Parity asset allocation.
+
+        Args:
+            assets (list): list of name of assets, if not specified then using the same list of assets when the class instance is initiated. 
+            method (str): method for optimization, default is Sequential Least Squares Programming (SLSQP). 
+            For full list of optimization method,
+                refer to [https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html]
+        
+        Attributes:
+            allocation_ (list): the Risk Parity asset allocation 
+            allocation_df_ (Pandas DataFrame): a Pandas DataFrame with 2 columns: 1. asset names and 2. their Risk Parity allocation 
+            minimised_val_ (float): the minimised value of the convex risk function 
+        """
         self.method = method
         if assets is None:
             assets=self.assets
@@ -65,9 +92,17 @@ class RiskParity():
         self.minimised_val_ = opti_result.fun
     
     def cal_risk_stats(self, assets=None):
-        """A post-optimisation method"""
-        """Calculate marginal risk contribution (MRC), risk contribution (RC)
-        and relative risk contribution (RRC) of the ith asset"""
+        """This method calculates marginal risk contribution (MRC), risk contribution (RC)
+        and relative risk contribution (RRC) of the ith asset.
+        
+        Args:
+            assets (list or array): list of name of assets, if not specified then using the same list of assets when the class instance is initiated. 
+
+        Attributes:
+            MRC_ (array): marginal risk contribution of the ith asset 
+            RC_ (array): risk contribution of the ith asset 
+            RRC_ (array): relative risk contribution of the ith asset 
+        """
         if assets is None:
             assets=self.assets
         w_rpp = self.allocation_
@@ -99,11 +134,19 @@ class RiskParity():
         self.RRC_ = RRC
     
     def visualise_risk_stats(self):
+        """
+        This method visualises the risk statistics from the cal_risk_stats method above.
+        
+        Attributes:
+            MRC_fig_ (matplotlib fig): matplotlib figure of the marginal risk contribution (MRC) of all assets in the portfolios
+            RC_fig_ (matplotlib fig): matplotlib figure of the risk contribution (RC) of all assets in the portfolios
+            RRC_fig_ (matplotlib fig): matplotlib figure of the relative risk contribution (RRC) of all assets in the portfolios
+        """
         import seaborn as sns
         import matplotlib.pyplot as plt
         # MRC_i visualisation
         MRC_fig = sns.barplot(y=self.MRC_, x=self.assets)
-        MRC_fig.set_title('Marginal risk contribution to total portfolio risk')
+        MRC_fig.set_title('Marginal risk contribution (MRC) to total portfolio risk')
         self.MRC_fig_ = MRC_fig
         plt.show()
         # RC_i visualisation
@@ -118,8 +161,11 @@ class RiskParity():
         plt.show()
         
     def out_excel(self, directory=None):
-        """Outputs an excel file with the asset allocation.
-        If the directory is not specified, the method will output the file in the current working directory"""
+        """This method outputs an excel file with the asset allocation.
+        
+        Args:
+            directory (str): the directory in which to output the Excel file.
+                If the directory is not specified, the method will output the file in the current working directory"""
         import os
         self.directory = directory
         if directory is None:
@@ -129,8 +175,20 @@ class RiskParity():
 
 # Child class of RiskParity
 class NonConvexRP(RiskParity):
+    """
+    This child class inherits all of methods from the parent RiskParity class, except risk_func() and optimize()
+        to account for the non-convex formulation of the optimization problem.
+    """
     def risk_func(self, w=None):
-        """Modified optimise function for non-convex problem formulation"""
+        """This is the modified optimise function for non-convex problem formulation
+        DISCLAIMER: as this does not take into account the long-only and capital budget constraints, this may not work 100%.
+
+        Args:
+            w (list or array): initial guess for Risk Parity asset allocation, if not specified the default will be equal weights
+
+        Returns:
+            risk_func (float): the calculated risk function
+        """
         if w is None:
             w = self.w_guess
         n = len(w)
@@ -138,9 +196,32 @@ class NonConvexRP(RiskParity):
         risks = w * (self.cov_mat.dot(w))
         norm_risks = risks / risk_budget
         g = np.tile(norm_risks, n) - np.repeat(norm_risks, n)
-        return np.sum(g**2) # This is the main function to minimise
+        """Example: assuming n = 2 and norm_risk is [a, b, c]
+                                np.tile -> [a, b, c, a, b, c]
+                                np.repeat -> [a, a, b, b, c, c]
+                                np.tile - np.repeat
+                                i = 1, 2, 3 ;  j = 1
+                                Taking all the 1, 2, 3 minus the 1
+                                i = 1, 2, 3 ;  j = 2
+                                and so on ...
+                                i = 1, 2, 3 ;  j = 3
+                                By taking np.tile - np.repeat, one gets all the permutations of N assets taken 2 at a time (pairs of i and j) 
+                                """
+        return np.sum(g**2) # This is the main function to minimise, a SUM OF SQUARES, (NOT SQUARE OF SUM) 
     
     def optimize(self, assets=None, method='BFGS'):
+        """This method minimize the risk function (risk_func) and displays a Risk Parity asset allocation.
+
+        Args:
+            assets (list): list of name of assets, if not specified then using the same list of assets when the class instance is initiated. 
+            method (str): method for optimization, default is the Broyden–Fletcher–Goldfarb–Shanno (BFGS). 
+            For full list of optimization method,
+                refer to [https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html]
+        
+        Attributes:
+            allocation_ (list): the Risk Parity asset allocation 
+            allocation_df_ (Pandas DataFrame): a Pandas DataFrame with 2 columns: 1. asset names and 2. their Risk Parity allocation 
+            minimised_val_ (float): the minimised value of the convex risk function"""
         self.method = method
         if assets is None:
             assets=self.assets
@@ -156,15 +237,33 @@ class NonConvexRP(RiskParity):
         self.allocation_df_ = allocation_df
         self.minimised_val_ = opti_result.fun
 
-# Designing back-end of the programme. Receiving csv -> Returning cov_mat
+# The preprocessing part of the programme. Receiving csv -> Returning cov_mat
 class PrepDataRP():
-    """This class aims to turn an imported csv file into a return covariance matrix (Matrix A)"""
+    """This class aims to turn an imported csv file into a return covariance matrix (Matrix A), which can be used as an input for the RiskParity or NonConvexRP classes above"""
     import pandas as pd
     import numpy as np
     def __init__(self, df=None):
+        """
+        Initiator
+
+        Args:
+            df (Pandas DataFrame): the DataFrame to preprocess 
+                It should have the following format:
+                    1 column with the dates
+                    n columns with the asset prices, with correct date-asset price combination
+        """
         self.df = df
         
     def transform(self, df=None):
+        """
+        This method transforms the correctly formatted Pandas DataFrame into a covariance matrix to be as an input for the RiskParity or NonConvexRP classes above.
+        
+        Attributes:
+            assets_ (list): the names of the assets i.e. the column names of the formatted Pandas DataFrame 
+
+        Returns:
+            cov_mat (array): the return covariance matrix, which can be used as an input for the RiskParity or NonConvexRP classes above 
+        """
         if df is None:
             df = self.df
         # Drop NaN values to make sure asset time series have the same # of observatives
@@ -183,7 +282,23 @@ class PrepDataRP():
         return cov_mat
 
 class api_data():
+    """
+    This class imports data from RapidAPI's Yahoo Finance API, then outputs a time series DataFrame with date and prices of selected assets.
+    """
     def __init__(self, api_key, assets, start, finish, freq):
+        """
+        Initiator
+
+        Args:
+            api_key (str): one's own API key. For instructions, refer to the Instruction notebook.
+            assets (list): List of ticker, must be valid and available on Yahoo finance
+            start (str): start date of time series, in the format of YYYY-MM-DD
+            finish (str): finish date of time series, in the format of YYYY-MM-DD
+            freq (str): the frequency of data:
+                  '1d': daily
+                  '1wk': weekly
+                  '1mo': monthly 
+        """
         self.api_key = api_key
         self.assets = assets # List of assets, to be looped later
         self.start = start
@@ -191,11 +306,27 @@ class api_data():
         self.freq = freq
     
     def time_to_unix(self, date, pattern='%Y.%m.%d'):
+        """
+        Helper method: converting real time to unix timestamps
+
+        Args:
+            date (str): date, in the format of YYYY-MM-DD by default
+            pattern (str): date pattern to process, by default it is '%Y.%m.%d'
+
+        Returns:
+            unix (int): equivalent timestamp item of date arg. 
+        """
         import time
         unix = int(time.mktime(time.strptime(date, pattern)))
         return unix
 
-    def get_api_data(self, api_key, asset, freq, start, finish):
+    def get_api_data(self, api_key, asset, start, finish, freq):
+        """
+        Helper method: getting raw data from RapidAPI's Yahoo Finance API
+        
+        Returns:
+            response_json (json): a json file containing historical data 
+        """
         import requests
         import json
         start = self.time_to_unix(self.start, pattern='%Y.%m.%d')
@@ -211,7 +342,17 @@ class api_data():
         response_json = json.loads(response.text)
         return response_json
 
-    def make_time_series(self, response_json, asset=None): # NEED TO FILL OUT THE ARGS
+    def make_time_series(self, response_json, asset=None):
+        """
+        Helper method: turning the response_json file into a time series DataFrame (with date and price columns)
+
+        Args:
+            response_json (json): a json file containing historical data
+            asset (str, optional): name of asset / ticker
+
+        Returns:
+            times_series_df (Pandas DataFrame): time series DataFrame
+        """
         from datetime import datetime
         import pandas as pd
         date_list_unix = []
@@ -228,7 +369,17 @@ class api_data():
         return time_series_df
 
     def prep_data(self, api_key=None, assets=None, 
-                  start=None, finish=None, freq=None): # Combined function, using the helper function
+                  start=None, finish=None, freq=None): 
+        """
+        Main function, using the helper methods. This method gets the data from the API, processes it and returns a DataFrame:
+            1. a date column
+            2. N price columns
+            3. n rows each containing an observation 
+            --> Shape: (n ,1+N)
+
+        Returns:
+            merged_df (Pandas DataFrame): the combined time series DataFrame
+        """
         if api_key is None:
             api_key = self.api_key
         if assets is None:
@@ -245,10 +396,10 @@ class api_data():
             # Go through the motions
             response_json = self.get_api_data(api_key=self.api_key, asset=asset, freq=self.freq, start=self.start, finish=self.finish)
             time_series_df = self.make_time_series(response_json=response_json, asset=asset)
-            time_series_dict[f"{asset}_df"] = time_series_df #For example MSFT_df is a time series dataframe with 2 cols: date and price
-            # Now we got a time series dictionary with security name key and their time series dataframe as value
+            time_series_dict[f"{asset}_df"] = time_series_df # For example MSFT_df is a time series DataFrame with 2 cols: date and price
+            # Now we got a time series dictionary with security name key and their time series DataFrame as value
             time_series_list.append(time_series_dict[f"{asset}_df"])
         merged_df = pd.concat(time_series_list, axis=1, join='outer')
         merged_df = merged_df.reindex(index=merged_df.index[::-1])
         merged_df = merged_df.loc[:, ~merged_df.columns.duplicated()]
-        return merged_d
+        return merged_df
